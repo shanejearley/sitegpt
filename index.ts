5ab67815e588cbe2e0ca6eb2707e08bb2c4a801e
Shane Earley <32200924+shanejearley@users.noreply.github.com>
2023-01-26 03:55:14 +0000

    Update max tokens

--- index.ts
+++ index.ts
@@ -12,11 +12,10 @@ const port = 3000
 app.get('/', async (_: express.Request, res: express.Response) => {
   const response = await openai.createCompletion({
     model: "code-davinci-002",
-    prompt: "<!-- Create a web page with the title 'SiteGPT', the tagline 'Website by OpenAI Codex', and a gradient background -->\n<!DOCTYPE html>",
+    prompt: "<!-- Create a web page with the title 'SiteGPT' -->\n<!DOCTYPE html>",
     temperature: 0,
-    max_tokens: 256 * 2
+    max_tokens: 256 * 4
   })
-  console.log(response.data.choices[0].text)
   res.send(response.data.choices[0].text)
 })
 
